6e5e0e1aac9af90035fef335f2df5ffc334f9708
Jay Lee <jay@home.jaylee.us>
2018-04-13 15:44:03 -0400

    Reduce permissions to work in PS, code cleanup

--- extension/popup.js
+++ extension/popup.js
@@ -1,28 +1,18 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-document.addEventListener('DOMContentLoaded', function() {
-  if (typeof chrome.enterprise == 'undefined' || typeof chrome.enterprise.deviceAttributes == 'undefined') {
-    
+function dictToURI(dict) {
+  var str = [];
+  for(var p in dict){
+     str.push(encodeURIComponent(p) + "=" + encodeURIComponent(dict[p]));
   }
-  chrome.enterprise.deviceAttributes.getDirectoryDeviceId(function (deviceid) {
-                console.log('Getting Device Id...');
-                console.log('Device Id: ' + deviceid);
-                chrome.storage.managed.get('primary_domain', function(primary_domain) {
-                  console.log('Managed Storage Domain: ' + primary_domain.primary_domain);
-                  chrome.identity.getProfileUserInfo(function(user_info) {
-                    identity_domain = user_info["email"].split("@")[1];
-                    console.log('Identity Domain: ' + identity_domain);
-                    if (typeof primary_domain.primary_domain == 'undefined') {
-                      use_domain = identity_domain;
-                    } else {
-                      use_domain = primary_domain.primary_domain;
-                    };
-                    appengine_url = 'https://cros-info.appspot.com/?primary_domain=' + use_domain + '&deviceid=' + deviceid;
-                    console.log("URL: " + appengine_url);
-                    document.getElementById("appengine").src = appengine_url;
-                    })
-                  });
-  });
-});
+  return str.join("&");
+}
+
+document.onreadystatechange = function () {
+  if (document.readyState === "interactive") {
+    chrome.extension.sendMessage({}, function(data) {
+      appengine_url = 'https://cros-info.appspot.com/?' +
+          dictToURI(data);
+      console.log("URL: " + appengine_url);
+      document.getElementById("appengine").src = appengine_url;
+    });
+  }
+};
